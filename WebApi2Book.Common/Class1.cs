69278feb43fed09a5611e78f0ecd97c814841e82
paul.bailey@Virtualcodestore.com <paul.bailey@Virtualcodestore.com>
2017-05-03 12:46:31 +0100

    Create Initial Projects

---
+++ WebApi2Book.Common/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi2Book.Common
+{
+    public class Class1
+    {
+    }
+}
