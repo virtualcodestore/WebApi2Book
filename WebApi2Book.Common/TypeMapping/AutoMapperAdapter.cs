b0c94ab8b70db0a1f560db86496e1a90cc14944d
paul.bailey@Virtualcodestore.com <paul.bailey@Virtualcodestore.com>
2017-05-09 15:43:09 +0100

    Added AutoMapper DI

---
+++ WebApi2Book.Common/TypeMapping/AutoMapperAdapter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace WebApi2Book.Common.TypeMapping
+{
+    public class AutoMapperAdapter :IAutoMapper
+    {
+        public T Map <T>( object objectToMap) 
+        { 
+            return Mapper.Map < T >( objectToMap); 
+        }
+
+
+    }
+}
